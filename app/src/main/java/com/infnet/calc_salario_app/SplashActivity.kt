2a4840b227f22da435196ab3d83d2278602c173d
viniOnRails <57204434+lanzgit@users.noreply.github.com>
2022-08-10 22:03:44 -0300

    Main/Splash things

---
+++ app/src/main/java/com/infnet/calc_salario_app/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.infnet.calc_salario_app
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+        lifecycleScope.launch{
+            while (true) {
+                delay(4000)
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                finish()
+                break
+        }}
+
+
+
+    }
+}
