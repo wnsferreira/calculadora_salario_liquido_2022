b65c7bf65d66756bb42e34663d87869502d02586
viniOnRails <57204434+lanzgit@users.noreply.github.com>
2022-09-19 16:39:48 -0300

    admob in result actv

--- app/src/main/java/com/infnet/calc_salario_app/ResultActivity.kt
+++ app/src/main/java/com/infnet/calc_salario_app/ResultActivity.kt
@@ -3,12 +3,23 @@ package com.infnet.calc_salario_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.AdView
 
 
 class ResultActivity : AppCompatActivity() {
+
+    private lateinit var mAdView: AdView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        MobileAds.initialize(this)
+        mAdView = this.findViewById(R.id.adView_result)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
 
         val txtSalarioLiquido = this.findViewById<TextView>(R.id.txtSalarioLiquido)
         val txtTotalDesconto = this.findViewById<TextView>(R.id.txtTotalDesconto)
